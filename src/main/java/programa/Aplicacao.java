1da8155ec59bc517cfadcde53678a806011b0749
luisagrosa <luisarosa95@gmail.com>
2021-03-23 21:40:05 -0300

    Depois de muitas tentativas, quase chegando na raiz do bug: sempre que chama o pedeNumeroItem(), ele precisa retornar

--- src/main/java/programa/Aplicacao.java
+++ src/main/java/programa/Aplicacao.java
@@ -9,26 +9,30 @@ public class Aplicacao {
 
     private static Set<Produto> produtosDisponiveis = new TreeSet<>();
     private static List<Estoque> estoqueDeProdutos;
+    private static int produtoDesejadofinal;
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
 
         criarProdutosEEstoqueInicial();
         mostrarProdutosParaUsuario();
         pedeNumeroItem();
-        //solicitaPagamento();
+        //solicitaPagamento(pedeNumeroItem());
         //dispensaProduto();
 
+
+
+        //retornarIntDeIDDeProdutoDisponivel___2();
     }
 
-    private static void mostrarProdutosParaUsuario(){
+    private static void mostrarProdutosParaUsuario() {
         System.out.println("\n--M-E-N-U---- Hoje temos os seguintes produtos para voce experimentar: ----M-E-N-U--");
-        for (Estoque e : estoqueDeProdutos)   {
-            if(e.getQuantidade() > 0) {
+        for (Estoque e : estoqueDeProdutos) {
+            if (e.getQuantidade() > 0) {
                 produtosDisponiveis.add(e.getProduto());
             }
         }
-        for (Produto p : produtosDisponiveis) {  // porque que esta permitindo duplicacao TODO
+        for (Produto p : produtosDisponiveis) {  // por que que esta permitindo duplicacao TODO
             System.out.println(p.getDescricaoCompleta());
         }
     }
@@ -37,22 +41,22 @@ public class Aplicacao {
     private static int pedeNumeroItem() {
         System.out.println("\nDigite o numero do item que deseja. Se deseja ver o MENU novamente, digite 0\n");
         int produtoDesejado = retornarIntDeIDDeProdutoDisponivel();
+
+        System.out.println("o produtoDesejado ficou com valor de:" + produtoDesejado);
         return produtoDesejado;
 
-        }
+    }
 
 
+    private static void criarProdutosEEstoqueInicial() {
 
 
-    private static void criarProdutosEEstoqueInicial(){
-
-
-        Produto doritos = new Produto((short)1, 3F, "Doritos", CategoriaProduto.SALGADO);
-        Produto cocaCola = new Produto((short)2, 5F, "Coca", CategoriaProduto.BEBIDA);
-        Produto trakinas = new Produto((short)3, 4F, "Trakinas", CategoriaProduto.DOCE);
-        Produto amendoim = new Produto((short)4, 1F, "Amendoim", CategoriaProduto.SALGADO);
-        Produto suco = new Produto((short)5, 8F, "Suquinho", CategoriaProduto.BEBIDA);
-        Produto chocolate = new Produto((short)6, 3F, "Chocolate", CategoriaProduto.DOCE);
+        Produto doritos = new Produto((short) 1, 3F, "Doritos", CategoriaProduto.SALGADO);
+        Produto cocaCola = new Produto((short) 2, 5F, "Coca", CategoriaProduto.BEBIDA);
+        Produto trakinas = new Produto((short) 3, 4F, "Trakinas", CategoriaProduto.DOCE);
+        Produto amendoim = new Produto((short) 4, 1F, "Amendoim", CategoriaProduto.SALGADO);
+        Produto suco = new Produto((short) 5, 8F, "Suquinho", CategoriaProduto.BEBIDA);
+        Produto chocolate = new Produto((short) 6, 3F, "Chocolate", CategoriaProduto.DOCE);
 
         Estoque estoquep1 = new Estoque(doritos, 10);
         Estoque estoquep2 = new Estoque(cocaCola, 10);
@@ -75,45 +79,71 @@ public class Aplicacao {
 
     }
 
+
     private static int retornarIntDeIDDeProdutoDisponivel() {
-        List<Integer>  idsDisponiveis = new ArrayList<>();
-        for (Produto p : produtosDisponiveis){
+        List<Integer> idsDisponiveis = new ArrayList<>();
+        for (Produto p : produtosDisponiveis) {
             idsDisponiveis.add(p.getId().intValue());
         }
 
 
-        int sc = 0;
         try {
-            sc = new Scanner(System.in).nextInt();
+           int sc = new Scanner(System.in).nextInt();
+            if (sc < 0) {
+                System.out.println("Você digitou um número negativo. Você precisa digitar um número de algum produto disponível");
+                pedeNumeroItem();
+            }
+            if (sc == 0) {
+                mostrarProdutosParaUsuario();
+                pedeNumeroItem();
+            }
+
+            if (!idsDisponiveis.contains(Integer.valueOf(sc))) {
+                System.out.printf("\nVocê digitou %d um id inexistente. Você precisa " +
+                        "digitar um número de algum produto disponível. São eles:", sc);
+                for (Integer i : idsDisponiveis) {
+                    System.out.print(i + ",");
+                }
+                pedeNumeroItem(); // parece que todas as vezes que a gente chama esse método de novo,
+                // ele "espera um resultado", entao por isso ele printa e retorna todos numeros que eu botei no console
+            }
+
+            return sc;
         }
 
         //eu queria poder voltar pro inicio do try caso desse essa exceção... como faz isso?? Instintivamente eu chamaria de novo
         //esse propprio metodo, mas nao pode ne ? porque fica "recursivo". Tem um jeito de voltar pro try ou pro inicio desse metodo
         // depois de informar o usuario no catch que ele tem que digitar um numero e nao uma letra ??? TODO
 
-        catch(InputMismatchException e) {
+        catch (InputMismatchException e) {
             System.out.println("Você não digitou um numero. Você precisa digitar um NÚMERO de algum produto disponível.");
             pedeNumeroItem();
         }
-        if(sc < 0){
-            System.out.println("Você digitou um número negativo. Você precisa digitar um número de algum produto disponível");
-            pedeNumeroItem();
-        }
-        if(sc == 0) {
-            mostrarProdutosParaUsuario();
-            pedeNumeroItem();
-        }
-        if(idsDisponiveis.contains(Integer.valueOf(sc))) {
-            return sc; }
-        else {
-            System.out.println("Você digitou um id inexistente. Você precisa digitar um número de algum produto disponível. São eles:");
-            for (Integer i : idsDisponiveis){
-                System.out.print(i + ",");
-            }
-            pedeNumeroItem();
-        } // por que as vezes eu ponho um id que existe e mesmo assim ele entende que nao existe?? TODO
 
-        return sc;
+        return pedeNumeroItem();
+    }
+
+
+
+
+
+
+
+
+    private static int retornarIntDeIDDeProdutoDisponivel___2() {
+        List<Integer> idsDisponiveis = new ArrayList<>();
+        for (Produto p : produtosDisponiveis) {
+            idsDisponiveis.add(p.getId().intValue());
+        }
+
+        Integer sc2 = new Scanner(System.in).nextInt();
+
+        while(!idsDisponiveis.contains(sc2)) {
+            System.out.println("\nDigite o numero do item que deseja. Se deseja ver o MENU novamente, digite 0\n");
+            sc2 = new Scanner(System.in).nextInt();
+
+        }
+        return sc2;
     }
 
 }
